1ea99a15b2ef9388f93f74f9cfe871829e9b5e9c
Chanvin Xiao <chanvinxiao@163.com>
2020-04-16 09:04:01 +0800

    add secret

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const { createServer } = require('http');
 const port = process.env.GITHUB_WEBHOOK_PORT || '3000';
+const secret = process.env.GITHUB_WEBHOOK_SECRET || '';
 
 const server = createServer((req, res) => {
   if('POST' === req.method){
@@ -8,6 +9,16 @@ const server = createServer((req, res) => {
       body += chunk.toString();
     });
     req.on('end', () => {
+      if('' !== secret){
+        const { createHmac } = require('crypto');
+        let signature = createHmac('sha1', secret).update(body).digest('hex');
+        if(req.headers['x-hub-signature'] !== `sha1=${signature}`){
+          console.log('Signature Error');
+          res.statusCode = 403;
+          res.end();
+          return;
+        }
+      }
       try{
         body = JSON.parse(decodeURIComponent(body).replace(/^payload=/, ''));
       }catch(e){
@@ -20,13 +31,13 @@ const server = createServer((req, res) => {
           const command = `cd ../${body.repository.name} && git pull origin master`
           exec(command, (error, stdout, stderr) => {
             if(error){
-              console.log(`updated unsuccessfully`);
+              console.log('updated unsuccessfully');
               console.log(error);
               res.statusCode = 500;
               res.end('Error');
             }else{
               console.log(stdout);
-              console.log(`updated successfully`);
+              console.log('updated successfully');
               res.statusCode = 200;
               res.end('Success');
             }
